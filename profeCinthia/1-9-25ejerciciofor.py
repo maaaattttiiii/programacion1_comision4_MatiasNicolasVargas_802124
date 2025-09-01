751b39abca3fedbbfca85f180f86685f6ff6415d
Tu Nombre <tuemail@ejemplo.com>
2025-09-01 09:54:03 -0300

    ejercico while

--- profeCinthia/1-9-25ejerciciofor.py
+++ profeCinthia/1-9-25ejerciciofor.py
@@ -1,7 +1,9 @@
+#EJERCICIO FOR 
+
 print("\n =========QUE MENSAJE QUIERE ENVIAR=========")
 alfabeto = "abcdefghijklmnñopqrstuvwxyz"
 
-corrimiento = int(input("Ingrese el corrimiento: "))
+corrimiento = int(input("Ingrese cuanto quiere que se corran las letras: "))
 
 for i in range(5):
     mensaje = input(f"Ingrese el mensaje {i+1}: ")
@@ -20,4 +22,56 @@ for i in range(5):
             encriptado += letra  
 
     print("Mensaje encriptado:", encriptado)
+
+
+#EJERCICIO WHILE
+
+import random
+
+while True:
+    print("\n==== QUE ELIGES ESTA RONDA ====")
+    print("1. Piedra")
+    print("2. Papel ")
+    print("3. Tijera")
+    print("4. No quiero jugar tengo miedo")
+
+    opcion = int(input("te espero...: "))
+
+    if opcion == 1:
+        opcion1 = random.randint(1,3)
+        if opcion1 == 2 :
+            print("looser")
+        elif opcion1 == opcion:
+            print("crack, pareciera que sos vos la maquina, sacamos lo mismo ")
+        else : 
+            print("aaa no, pero vos sos una bestia")
+            
+    elif opcion == 2:
+        opcion1 = random.randint(1,3)
+        if opcion1 == 3 :
+            print("looser")
+        elif opcion1 == opcion: 
+            print("crack, pareciera que sos vos la maquina, sacamos lo mismo ")
+        else: 
+            print("pero estamos hablando de un prodigio me parece, que capo")
+            
+    elif opcion == 3:
+        opcion1 = random.randint(1,3)
+        if opcion1 == 1 :
+            print("looser")
+        elif opcion1 == opcion: 
+            print("crack, pareciera que sos vos la maquina, sacamos lo mismo ")
+        else: 
+            print("che... me sorprenden tus habilidades en este jueguito")
+        
+        
+        
+    elif opcion == 4:
+        print("Saliendo entonces...")
+        break
+
+    else:
+        print("❌ Opción no válida")
+
+
 
